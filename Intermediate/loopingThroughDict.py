cbd29256e35f6d23ec81695ed87ff631aedd63d6
Teddy Pascual <teddylayson2000@gmail.com>
2024-03-12 21:06:44 +0800

    add looping through dictionaries lesson

---
+++ Intermediate/loopingThroughDict.py
@@ -0,0 +1,20 @@
+exam_scores = {
+    'Jonathan': 85,
+    'Erica': 99,
+    'Rose': 83,
+    'Henry': 92,
+    'Genesis': 75,
+    'Teddy': 97,
+    'Maureene': 98,
+    'Tyga': 65,
+}
+# .key .values
+for key, value in exam_scores.items():
+    print(key)
+    print(value)
+    print('-------')
+
+my_programmingLanguage = {
+    1: {'language': 'Java'}, 2:  {'language': 'Javascript'}
+}
+print(my_programmingLanguage[1]["language"])
