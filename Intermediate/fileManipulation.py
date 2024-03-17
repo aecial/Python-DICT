a814139d8d56d3a294b7fc5b0d95f6aabc245190
Teddy Pascual <teddylayson2000@gmail.com>
2024-03-17 09:11:45 +0800

    add file manipulation

---
+++ Intermediate/fileManipulation.py
@@ -0,0 +1,17 @@
+# create a file
+# f = open('newfile.txt', 'x')
+
+# write to a file
+# w will overwrite
+# f = open('newfile.txt', 'w')
+# f.write('Python is Fun!')
+
+# read a file
+# f = open('newfile.txt', 'r')
+# print(f.read())
+# f.close()
+
+# append or add to a file
+# f = open('newfile.txt', 'a')
+# f.write('\nDjango is my Fav')
+# f.close()
